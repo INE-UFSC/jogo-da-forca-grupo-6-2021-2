a40304711a13ed690111a5af8e3f1f4929a3d269
Felipe Kettl <fekettl@gmail.com>
2021-10-29 09:52:54 -0300

    Comentei a função

--- jogo.py
+++ jogo.py
@@ -1,5 +1,7 @@
-def acerto(letra, resposta):
-    
+def acerto(letra, resposta): #Felipe Backes Kettl
+    #Função para verificar o acerto, ou não, da letra
+    #Recebe a letra que o jogador escolheu e a palavra-resposta que o jogo selecionou
+    #Retorna o bool se acertou ou não e uma lista com os index dos acertos
     acertou = False
     index = []
     for x in range(len(resposta)):
