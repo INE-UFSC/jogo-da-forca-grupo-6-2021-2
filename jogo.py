e10d202616c36eb2f2f4edd793bd033f67a8a554
Felipe Kettl <fekettl@gmail.com>
2021-10-29 09:58:25 -0300

    Refazendo função

--- jogo.py
+++ jogo.py
@@ -8,3 +8,16 @@ def atualiza_palavra(palavra: str, letra: str, indices: list):
     palavra = "".join(palavra)
 
     return palavra
+
+def acerto(letra, resposta): #Felipe Backes Kettl
+    #Função para verificar o acerto, ou não, da letra
+    #Recebe a letra que o jogador escolheu e a palavra-resposta que o jogo selecionou
+    #Retorna o bool se acertou ou não e uma lista com os index dos acertos
+    acertou = False
+    index = []
+    for x in range(len(resposta)):
+        if letra == resposta[x]:
+            index.append(x)
+            acertou = True
+    return acertou, index
+
