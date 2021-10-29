a0ba794107d5d63bf9dd1c4f66fe65379907c52d
sammycosta <86506839+sammycosta@users.noreply.github.com>
2021-10-29 10:02:48 -0300

    Arquivo com boneco e palavras + função exibe_boneco

---
+++ jogo.py
@@ -0,0 +1,9 @@
+from forca import boneco, lista_palavras
+
+
+def exibe_boneco(vidas_restantes: bool):  # Samantha
+    'Recebe vidas_restantes(bool) e retorna o estado atual do boneco da forca(str)'
+    return (boneco[vidas_restantes])
+
+
+print(exibe_boneco(0))
