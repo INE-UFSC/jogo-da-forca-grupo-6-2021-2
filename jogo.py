43671b37792ee73cafe9f041c70c6452b6450279
Felipe Kettl <fekettl@gmail.com>
2021-10-29 10:11:20 -0300

    refazendo tudo

--- jogo.py
+++ jogo.py
@@ -6,4 +6,28 @@ def exibe_boneco(vidas_restantes: bool):  # Samantha
     return (boneco[vidas_restantes])
 
 
+def atualiza_palavra(palavra: str, letra: str, indices: list):
+    letra = letra.upper()
+    palavra = list(palavra.upper())
+
+    for index in indices:
+        palavra[index] = letra
+
+    palavra = "".join(palavra)
+
+    return palavra
+
+def acerto(letra, resposta): #Felipe Backes Kettl
+    #Função para verificar o acerto, ou não, da letra
+    #Recebe a letra que o jogador escolheu e a palavra-resposta que o jogo selecionou
+    #Retorna o bool se acertou ou não e uma lista com os index dos acertos
+    acertou = False
+    index = []
+    for x in range(len(resposta)):
+        if letra == resposta[x]:
+            index.append(x)
+            acertou = True
+    return acertou, index
+
+
 
